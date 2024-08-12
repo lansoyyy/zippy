bf5efb50229246cc777bc386b5e034db452a17cf
Lans12345 <olanalans12345@gmail.com>
2024-08-12 18:38:18 +0800

    changed app name and app bundle

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,8 +2,9 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-const primary = Colors.orange;
+const secondary = Color(0XFFFF7300);
 
-var secondary = const Color(0xffFCB315);
-var black = const Color(0xff151515);
+const primary = Color(0xffff4b02);
+var black = const Color(0xff1b1b1b);
+var white = const Color(0xffffffff);
 var grey = const Color(0xffB3B3B3);
