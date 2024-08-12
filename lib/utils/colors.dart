b514b6e2d0068aef998027ca2ed8af17b6eac87c
Lans12345 <olanalans12345@gmail.com>
2024-08-12 18:30:29 +0800

    initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const primary = Colors.orange;
+
+var secondary = const Color(0xffFCB315);
+var black = const Color(0xff151515);
+var grey = const Color(0xffB3B3B3);
