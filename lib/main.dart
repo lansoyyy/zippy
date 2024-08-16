ffeec6500c561bf083a12ee7217eecb9890adbfe
Lans12345 <olanalans12345@gmail.com>
2024-08-16 19:43:31 +0800

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Zippy',
+      title: 'Zippys',
       home: SplashScreen(),
     );
   }
