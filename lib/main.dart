386c9e79164f006b9846c096119f373ea8acd9e6
Lans12345 <olanalans12345@gmail.com>
2024-08-16 19:43:42 +0800

    fixed typo

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Zippys',
+      title: 'Zippy',
       home: SplashScreen(),
     );
   }
