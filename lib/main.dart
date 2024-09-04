eea4f764d3255f378eedc46c5ebfbf630b2b906a
Lans12345 <olanalans12345@gmail.com>
2024-09-04 18:34:16 +0800

    wip onboarding screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zippy/screens/auth/landing_screen.dart';
 import 'package:zippy/screens/home_screen.dart';
+import 'package:zippy/screens/onboarding_screens/onboarding_one.dart';
 import 'package:zippy/screens/splash_screen.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Zippy',
-      home: SplashScreen(),
+      home: OnboardingOne(),
     );
   }
 }
