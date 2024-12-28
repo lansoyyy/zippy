0cdffe038abfade0839b514adc0df0b074aa7aac
Lans12345 <olanalans12345@gmail.com>
2024-12-28 14:12:49 +0800

    added search page func

--- lib/screens/pages/search_page.dart
+++ lib/screens/pages/search_page.dart
@@ -1,11 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' show DateFormat, toBeginningOfSentenceCase;
+import 'package:zippy/screens/pages/shop_page.dart';
 import 'package:zippy/utils/colors.dart';
-import 'package:zippy/utils/const.dart';
 import 'package:zippy/widgets/text_widget.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final searchController = TextEditingController();
+  String nameSearched = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,13 +99,19 @@ class SearchPage extends StatelessWidget {
                   ),
                   borderRadius: BorderRadius.circular(5.0),
                 ),
-                child: const Row(
+                child: Row(
                   children: [
-                    Icon(Icons.search, color: Colors.black54),
-                    SizedBox(width: 8.0),
+                    const Icon(Icons.search, color: Colors.black54),
+                    const SizedBox(width: 8.0),
                     Expanded(
                       child: TextField(
-                        decoration: InputDecoration(
+                        onChanged: (value) {
+                          setState(() {
+                            nameSearched = value;
+                          });
+                        },
+                        controller: searchController,
+                        decoration: const InputDecoration(
                             border: InputBorder.none,
                             hintText: 'What are you craving today?',
                             hintStyle: TextStyle(
@@ -106,7 +121,7 @@ class SearchPage extends StatelessWidget {
                             )),
                       ),
                     ),
-                    Icon(Icons.tune, color: Colors.black54),
+                    // Icon(Icons.tune, color: Colors.black54),
                   ],
                 ),
               ),
@@ -114,155 +129,186 @@ class SearchPage extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                for (int i = 0; i < foodCategories.length; i++)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5, right: 5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: i == 0 ? secondary.withOpacity(0.3) : null,
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: secondary,
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Image.asset(
-                              foodCategories[i],
-                              width: 25,
-                              height: 25,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        TextWidget(
-                          text: foodCategoriesName[i],
-                          fontSize: 12,
-                          fontFamily: 'Medium',
-                          color: secondary,
-                        ),
-                      ],
-                    ),
-                  ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            for (int i = 0; i < 10; i++)
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: GestureDetector(
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Card(
-                          elevation: 3,
-                          child: Container(
-                            width: 80,
-                            height: 132,
-                            decoration: BoxDecoration(
-                              image: const DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage(
-                                  'assets/images/Rectangle 2.png',
-                                ),
-                              ),
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                color: secondary,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Card(
-                          elevation: 3,
-                          child: Container(
-                            width: 230,
-                            height: 132,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                color: secondary,
-                              ),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     for (int i = 0; i < foodCategories.length; i++)
+            //       Padding(
+            //         padding: const EdgeInsets.only(left: 5, right: 5),
+            //         child: Column(
+            //           crossAxisAlignment: CrossAxisAlignment.center,
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           children: [
+            //             Container(
+            //               decoration: BoxDecoration(
+            //                 color: i == 0 ? secondary.withOpacity(0.3) : null,
+            //                 shape: BoxShape.circle,
+            //                 border: Border.all(
+            //                   color: secondary,
+            //                 ),
+            //               ),
+            //               child: Padding(
+            //                 padding: const EdgeInsets.all(15.0),
+            //                 child: Image.asset(
+            //                   foodCategories[i],
+            //                   width: 25,
+            //                   height: 25,
+            //                 ),
+            //               ),
+            //             ),
+            //             const SizedBox(
+            //               height: 10,
+            //             ),
+            //             TextWidget(
+            //               text: foodCategoriesName[i],
+            //               fontSize: 12,
+            //               fontFamily: 'Medium',
+            //               color: secondary,
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //   ],
+            // ),
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('Merchant')
+                    .where('businessName',
+                        isGreaterThanOrEqualTo:
+                            toBeginningOfSentenceCase(nameSearched))
+                    .where('businessName',
+                        isLessThan:
+                            '${toBeginningOfSentenceCase(nameSearched)}z')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    print(snapshot.error);
+                    return const Center(child: Text('Error'));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Center(
+                          child: CircularProgressIndicator(
+                        color: Colors.black,
+                      )),
+                    );
+                  }
+
+                  final data = snapshot.requireData;
+                  return Column(
+                    children: [
+                      for (int i = 0; i < data.docs.length; i++)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: GestureDetector(
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => ShopPage(
+                                        merchantId: data.docs[i]['uid']),
+                                  ),
+                                );
+                              },
+                              child: Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  TextWidget(
-                                    text: 'Jollibee Downtown Strip',
-                                    fontSize: 16,
-                                    fontFamily: 'Bold',
+                                  Card(
+                                    elevation: 3,
+                                    child: Container(
+                                      width: 80,
+                                      height: 132,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                            data.docs[i]['img'],
+                                          ),
+                                        ),
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                          color: secondary,
+                                        ),
+                                      ),
+                                    ),
                                   ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      const Icon(
-                                        Icons.delivery_dining_outlined,
-                                        size: 15,
-                                      ),
-                                      const SizedBox(
-                                        width: 5,
-                                      ),
-                                      TextWidget(
-                                        text: 'Free delivery',
-                                        fontSize: 10,
-                                        fontFamily: 'Regular',
-                                      ),
-                                      const SizedBox(
-                                        width: 15,
-                                      ),
-                                      const Icon(
-                                        Icons.access_time_sharp,
-                                        size: 15,
-                                      ),
-                                      const SizedBox(
-                                        width: 5,
-                                      ),
-                                      TextWidget(
-                                        text: '10-15 minutes',
-                                        fontSize: 10,
-                                        fontFamily: 'Regular',
-                                      ),
-                                    ],
+                                  const SizedBox(
+                                    width: 5,
                                   ),
-                                  TextWidget(
-                                    text: 'Fastfood • Chicken • Burger • Pasta',
-                                    fontSize: 16,
-                                    align: TextAlign.start,
-                                    fontFamily: 'Bold',
-                                    color: secondary,
-                                  ),
-                                  TextWidget(
-                                    text: 'Price range ₱',
-                                    fontSize: 10,
-                                    fontFamily: 'Regular',
-                                  ),
-                                  Align(
-                                    alignment: Alignment.bottomRight,
-                                    child: TextWidget(
-                                      text: '20% off first purchase. min ₱500',
-                                      fontSize: 10,
-                                      fontFamily: 'Regular',
+                                  Card(
+                                    elevation: 3,
+                                    child: Container(
+                                      width: 230,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                          color: secondary,
+                                        ),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            TextWidget(
+                                              text: data.docs[i]
+                                                  ['businessName'],
+                                              fontSize: 16,
+                                              fontFamily: 'Bold',
+                                            ),
+                                            TextWidget(
+                                              align: TextAlign.start,
+                                              text: data.docs[i]['desc'],
+                                              fontSize: 10,
+                                              fontFamily: 'Regular',
+                                            ),
+                                            Wrap(
+                                              children: [
+                                                for (int j = 0;
+                                                    j <
+                                                        data
+                                                            .docs[i]
+                                                                ['categories']
+                                                            .length;
+                                                    j++)
+                                                  TextWidget(
+                                                    text: data.docs[i]
+                                                            ['categories'][j] +
+                                                        ' • ',
+                                                    fontSize: 16,
+                                                    align: TextAlign.start,
+                                                    fontFamily: 'Bold',
+                                                    color: secondary,
+                                                  ),
+                                              ],
+                                            ),
+                                            TextWidget(
+                                              text: 'Price range ₱20',
+                                              fontSize: 10,
+                                              fontFamily: 'Regular',
+                                            ),
+                                            Align(
+                                              alignment: Alignment.bottomRight,
+                                              child: TextWidget(
+                                                text:
+                                                    '20% off first purchase. min ₱500',
+                                                fontSize: 10,
+                                                fontFamily: 'Regular',
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ],
@@ -270,11 +316,9 @@ class SearchPage extends StatelessWidget {
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+                    ],
+                  );
+                }),
           ],
         ),
       ),
