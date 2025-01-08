a0f82df09facb974fd8c278429d48b12cb54d82a
neilpagsss14 <neilpagara50@gmail.com>
2025-01-08 17:55:37 +0800

    fixed: amount to be paid

--- lib/screens/pages/order/checkout_page.dart
+++ lib/screens/pages/order/checkout_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:zippy/screens/pages/order/completed_page.dart';
@@ -8,6 +9,7 @@ import 'package:zippy/screens/pages/search_page.dart';
 import 'package:zippy/utils/colors.dart';
 import 'package:zippy/utils/const.dart';
 import 'package:zippy/widgets/text_widget.dart';
+import 'package:zippy/widgets/toast_widget.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({super.key});
@@ -23,12 +25,34 @@ class CheckoutPage extends StatefulWidget {
 
 class _CheckoutPageState extends State<CheckoutPage> {
   @override
+  Map<String, dynamic>? userData;
+  String? profileImage;
+  @override
   void initState() {
-    // TODO: implement initState
-
     showDialogs();
-
+    fetchUser();
     super.initState();
+  }
+
+  Future<void> fetchUser() async {
+    try {
+      DocumentReference userDoc =
+          FirebaseFirestore.instance.collection('Users').doc(userId);
+
+      userDoc.snapshots().listen((docSnapshot) {
+        if (docSnapshot.exists) {
+          final data = docSnapshot.data() as Map<String, dynamic>;
+          setState(() {
+            userData = data;
+            profileImage = data['profile'];
+          });
+        } else {
+          showToast('User data not found.');
+        }
+      });
+    } catch (e) {
+      print("Error fetching user data: $e");
+    }
   }
 
   showDialogs() async {
@@ -84,7 +108,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                           width: 275,
                           child: TextWidget(
                             align: TextAlign.start,
-                            text: 'Hi! Paula, Welcome Back!',
+                            text: 'Hi! ${userData!['name']}, Welcome Back!',
                             fontSize: 22,
                             fontFamily: 'Bold',
                             color: Colors.white,
@@ -97,12 +121,14 @@ class _CheckoutPageState extends State<CheckoutPage> {
                                   builder: (context) => const ProfilePage()),
                             );
                           },
-                          child: const CircleAvatar(
+                          child: CircleAvatar(
                             maxRadius: 25,
                             minRadius: 25,
-                            backgroundImage: AssetImage(
-                              'assets/images/sample_avatar.png',
-                            ),
+                            backgroundImage: profileImage != null
+                                ? NetworkImage(profileImage!)
+                                : const AssetImage(
+                                        'assets/images/Group 121 (2).png')
+                                    as ImageProvider,
                           ),
                         ),
                       ],
