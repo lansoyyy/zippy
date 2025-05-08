56ef07ea4094942b1f0c9d945a41bd21bea7a15d
Lans12345 <olanalans12345@gmail.com>
2025-05-08 08:56:15 +0800

    updated api key

--- lib/utils/keys.dart
+++ lib/utils/keys.dart
@@ -1,5 +1,5 @@
 const apiKey = 'AIzaSyDCNjDB8Gv4tET5cnE-gcWihA265JOlCVM';
 
-const kGoogleApiKey = 'AIzaSyDCNjDB8Gv4tET5cnE-gcWihA265JOlCVM';
+const kGoogleApiKey = 'AIzaSyArGfsiZq0QL1nLRdb2cDcOJQISQmh3SCg';
 
 const txboxKey = '2532a44b417825620e22a19a5dcb9b90';
